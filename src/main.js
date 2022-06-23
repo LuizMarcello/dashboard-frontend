d0f2d2d176322f7ee929816b8393dac440e5bc1e
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-06-22 23:11:14 -0300

    Londrina:22.06.22_Inicio

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import {
+  createApp
+} from 'vue'
+
+import App from './App.vue'
+
+import {
+  createRouter,
+  createWebHashHistory
+} from 'vue-router'
+
+import LoginComponent from './pages/Login/LoginComponent'
+import HomeComponent from './pages/Home/HomeComponent'
+
+const routes = [{
+    path: '/',
+    name: 'login', //localhost:8080
+    component: LoginComponent
+  },
+  {
+    path: '/home',
+    name: 'home', //localhost:8080/home
+    component: HomeComponent
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes // routes: routes
+})
+
+const Vue = createApp(App)
+Vue.use(router) //Adicionando as configurações 
+//de rotas a instância do Vue
+Vue.mount('#app')
+
+
